a8fefb3d736f35b1de872737d6e39e14fbdb4930
[YOUNES ADI] <younesadi18@gmail.com>
2024-11-10 15:06:22 +0100

    Back button,create account button and login button

--- app/src/main/java/com/example/myapp/CreateAccount.kt
+++ app/src/main/java/com/example/myapp/CreateAccount.kt
@@ -2,10 +2,22 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.content.Intent
 
 class CreateAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        val myTextViewButton = findViewById<TextView>(R.id.loginButton)
+
+        myTextViewButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myTextViewButton = findViewById<TextView>(R.id.RegisterButton)
+
+        myTextViewButton.setOnClickListener {
+            val intent = Intent(this, CreateAccount::class.java)
+            startActivity(intent)
+        }
     }
 }
